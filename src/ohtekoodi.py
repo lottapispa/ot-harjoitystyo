fc27be037fb2ba3042a0cb2086081e08db38a36d
Lotta Pispa <lotta.pispa@helsinki.fi>
2022-12-09 14:20:32 +0200

    move funktion toiminta

--- src/ohtekoodi.py
+++ src/ohtekoodi.py
@@ -2,7 +2,6 @@ import random
 import sys
 import pygame
 
-pygame.init()
 # Global variables
 screen_width = 640
 screen_height = 480
@@ -10,8 +9,8 @@ up = (0, -1)
 down = (0, 1)
 left = (-1, 0)
 right = (1, 0)
+step = 20 # how much snake moves at once
 pygame.display.set_caption("Snake")
-screen = pygame.display.set_mode((screen_width, screen_height))
 
 class Snake():
     def __init__(self):
@@ -26,42 +25,45 @@ class Snake():
     def head_location(self):
         return self.location[0]
 
-    def draw_snake(self, x, y):
-        # 20 is the size of the snake, x & y the location
-        pygame.draw.rect(screen, self.color, pygame.Rect(x, y, 20, 20))
+    def draw_snake(self, screen):
+        for location in self.location:
+            pygame.draw.rect(screen, self.color, pygame.Rect((location[0], location[1]), (20, 20)))
 
     def turn_up(self):
         if self.direction != down or self.direction != up or self.length == 1:
             self.direction = up
-            self.move()
 
     def turn_down(self):
         if self.direction != up or self.direction != down or self.length == 1:
             self.direction = down
-            self.move()
 
     def turn_left(self):
         if self.direction != right or self.direction != left or self.length == 1:
             self.direction = left
-            self.move()
 
     def turn_right(self):
         if self.direction != left or self.direction != right or self.length == 1:
             self.direction = right
-            self.move()
 
     def move(self):
+        #käärmettä liikutetaan niin, että käärmeeseen lisätään eteen yksi ruutu ja lopusta poistetaan yksi ruutu
         self.current = self.head_location()
         x = self.direction[0]
         y = self.direction[1]
+        # self.new_tuple on uusi head_location
         if self.direction == up or self.direction == down:
-            self.current[1] += y
+            self.new_tuple = (self.current[0], self.current[1] + (step * y))
         elif self.direction == left or self.direction == right:
-            self.current[0] += x
-        # jos liikkuessa käärmeen pää osuu sen vartaloon, se kuolee
-        if self.current in self.location[2:]:
+            self.new_tuple = (self.current[0] + (step * x), self.current[1])
+        # jos liikkuessa käärmeen pää osuu sen vartaloon tai seinään, se kuolee
+        if len(self.location) > 2 and self.new_tuple in self.location[2:]:
             self.die()
-        pygame.display.flip()
+        elif self.new_tuple[0] + 20 >= 640 or self.new_tuple[0] < 0 or self.new_tuple[1] + 20 >= 480 or self.new_tuple[1] < 0:
+            self.die()
+        else:
+            self.location.insert(0, self.new_tuple)
+            if len(self.location) >= self.length:
+                self.location.pop()
 
     def keyboard(self):
         for event in pygame.event.get():
@@ -80,6 +82,7 @@ class Snake():
     def die(self):
         # Game over window
         pygame.init()
+        screen = pygame.display.set_mode((screen_width, screen_height))
         screen.fill((255, 248, 220))  # cream white
         pygame.draw.rect(screen, (0, 0, 0),
                          (220, 100, 200, 290))  # color black
@@ -117,21 +120,22 @@ class Snake():
     def reset(self):
         # reset snake to original values for new game
         self.length = 1
-        self.location = [(screen_width/2), (screen_height/2)]  # lista
+        self.location = [((screen_width/2), (screen_height/2))]  # lista
         self.direction = random.choice([up, down, left, right])
         self.points = 0
 
 class Food():
     def __init__(self):
-        self.size = 8 # radius
+        self.snake = Snake()
+        self.size = 10 # radius
         self.color = (139,69,19)
         self.location = (0,0)
         self.random_location()
 
     def random_location(self):
-        self.location = (random.randint(3, 640-3), random.randint(3, 480-3))
+        self.location = (random.randint(10, 640-10), random.randint(10, 480-10))
 
-    def draw_food(self):
+    def draw_food(self, screen):
         pygame.draw.circle(screen, self.color, self.location, self.size)
 
     # how often does food show up on screen
@@ -140,27 +144,23 @@ class Food():
 
     # checks if snake and food collide and add length & points
     def eating(self):
-        if Snake().head_location() == self.location:
-            Snake().length += 1
-            Snake().points += 1
+        if self.snake.head_location() == self.location:
+            self.snake.length += 1
+            self.snake.points += 1
             self.random_location()
 
 def main():
     pygame.init()
-    x = (screen_width/2)
-    y = (screen_height/2)
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    screen.fill((255, 248, 220))  # cream white
+    snake = Snake()
     clock = pygame.time.Clock()
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill((255, 248, 220))  # cream white
-        Snake().draw_snake(x, y)
+        snake.keyboard()
+        snake.move()
+        snake.draw_snake(screen)
         pygame.display.flip()
-        x += 1
-        if x + 20 >= 640:
-            break
-        clock.tick(60)
-    Snake().die()
+        clock.tick(10)
 
-main()
+if __name__ == "__main__":
+    main()
